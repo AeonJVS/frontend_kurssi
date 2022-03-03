42d6afb3e70e0fc07c6c46cc3d1fb02c16df4380
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2022-03-03 17:48:07 +0200

    Tehtävä 16 - ToDoList poisto-toiminnolla tehty

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,8 @@ import React, {useState} from 'react';
 import TodoTable from './TodoTable';
 
 const TodoList = () => {
-    const [todos, setTodos] = useState([{}]);
+    const [todos, setTodos] = useState([]);
     const [todoEntry, setTodoEntry] = useState({description: "", date: ""})
-    const [update, setUpdate] = useState(false);
-
-    //const ToggleUpdate = setUpdate((state) => !state);
 
     const inputChanged = (event) => {
         setTodoEntry({...todoEntry, [event.target.name]: event.target.value});
@@ -15,7 +12,6 @@ const TodoList = () => {
     const addTodo = (event) => {
         event.preventDefault();
         setTodos([...todos, todoEntry]);
-        console.log(todos);
     }
 
     return (
@@ -28,7 +24,7 @@ const TodoList = () => {
                 <input type="text" name="date" value={todoEntry.date} onChange={inputChanged} />
                 <input type="submit" value="Add" />
             </form>
-            <TodoTable todos={todos} />
+            <TodoTable todos={todos} setTodos={setTodos} />
         </div>
     )
 }
--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const TodoTable = (props) => {
+
+    const deleteEntry = (props, index) => {
+        props.setTodos(props.todos.filter((todoEntry, i) => i !== index));
+    }
+
     return (
         <div>
             <table>
@@ -9,7 +14,8 @@ const TodoTable = (props) => {
                         <th>Date</th><th>Description</th>
                     </tr>
                     { props.todos.map((item, index) =>
-                    <tr key={index}><td>{item.date}</td><td>{item.description}</td></tr>) }
+                    <tr key={index}><td>{item.date}</td><td>{item.description}</td>
+                    <td><button onClick={() => deleteEntry(props, index)}>Delete</button></td></tr>)}
                 </tbody>
             </table>
         </div>
